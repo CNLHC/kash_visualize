e23002b5e5d037d174a69c7703cab7c3e5b15fd8
LIU HANCHENG <liuhancheng.cn@gmail.com>
2024-04-10 21:15:31 +0800

    Update settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@w&23qscp87nk7(*)-8+mtcc)zbx3m8^l8b(+4aemb&pub*19-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
